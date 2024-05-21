7cef3d1c6a6ef53d6c75e01a4295692ba84e391f
cottonstartiet <cottonstartiet@gmail.com>
2024-05-21 13:22:30 +0530

    Record sample

---
+++ RecordBenchmark/RecordBenchmark/Models/Address.cs
@@ -0,0 +1,9 @@
+﻿namespace RecordBenchmark;
+
+public record class Address
+{
+  public string Street { get; init; }
+  public string City { get; init; }
+  public string State { get; init; }
+  public string Zip { get; init; }
+}
---
+++ RecordBenchmark/RecordBenchmark/Models/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace RecordBenchmark.Models
+{
+    public record Customer
+    {
+        string Id { get; init; }
+        string Name { get; init; }
+        string Address { get; init; }
+
+    }
+}
