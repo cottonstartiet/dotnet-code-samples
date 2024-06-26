f3bb519d8a50de3432a56457e19e94863cee013f
cottonstartiet <cottonstartiet@gmail.com>
2024-06-26 12:48:52 +0530

    Make async await based

---
+++ CosmosDBSamples/CosmosDBSamples/CosmosDbService.cs
@@ -0,0 +1,39 @@
+﻿using CosmosDBSamples.DAL.Models;
+using Microsoft.Azure.Cosmos;
+using System.Threading.Tasks;
+
+namespace CosmosDBSamples
+{
+    internal class CosmosDbService
+    {
+        private readonly Database database;
+        private readonly Container jobsContainer;
+        public CosmosDbService()
+        {
+            CosmosClient client = new("");
+
+            database = client.CreateDatabaseIfNotExistsAsync("JobDatabase").Result;
+            jobsContainer = database.CreateContainerIfNotExistsAsync("JobContainer", "/pk").Result.Container;
+        }
+
+        public async Task<ItemResponse<JobInfo>> CreateJob(JobInfo jobInfo)
+        {
+            return await jobsContainer.UpsertItemAsync(jobInfo, new PartitionKey(jobInfo.id));
+        }
+
+        public Task<ItemResponse<JobInfo>> GetJobData(string jobId)
+        {
+            return jobsContainer.ReadItemAsync<JobInfo>(jobId, new PartitionKey(jobId));
+        }
+
+        public Task<ItemResponse<JobStatus>> SetJobStatus(JobStatus jobStatus, string jobId)
+        {
+            return jobsContainer.UpsertItemAsync(jobStatus, new PartitionKey(jobId));
+        }
+
+        public Task<ItemResponse<JobStatus>> GetJobStatus(string jobId)
+        {
+            return jobsContainer.ReadItemAsync<JobStatus>("STATUS", new PartitionKey(jobId));
+        }
+    }
+}
--- CosmosDBSamples/CosmosDBSamples/Program.cs
+++ CosmosDBSamples/CosmosDBSamples/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using CosmosDBSamples.DAL;
+using CosmosDBSamples;
 using CosmosDBSamples.DAL.Models;
 
 Console.WriteLine("Running cosmosdb single table design pattern sample");
@@ -7,28 +7,34 @@ Console.WriteLine("Running cosmosdb single table design pattern sample");
 string JOB_ID = "1";
 
 CosmosDbService service = new();
-service.CreateJob(new JobInfo { pk = JOB_ID, id = JOB_ID, JobName = "job-name-1", JobDescription = "job-description-1" });
 
-Console.WriteLine(service.GetJobData(JOB_ID));
+await service.CreateJob(new JobInfo { pk = JOB_ID, id = JOB_ID, JobName = "job-name-1", JobDescription = "job-description-1" });
 
-Console.WriteLine(service.SetJobStatus(
+var jobInfoResponse = await service.GetJobData(JOB_ID);
+Console.WriteLine(jobInfoResponse.Resource);
+
+var updateStatusResponse = await service.SetJobStatus(
     new JobStatus
     {
         pk = JOB_ID,
         id = "STATUS",
         Status = JobState.CREATED,
         UpdatedAt = DateTime.Now
-    }, JOB_ID));
+    }, JOB_ID);
+Console.WriteLine(updateStatusResponse.Resource);
 
-Console.WriteLine(service.GetJobStatus(JOB_ID)); ;
+var updatedJobStatus = await service.GetJobStatus(JOB_ID);
+Console.WriteLine(updatedJobStatus.Resource);
 
-Console.WriteLine(service.SetJobStatus(
+var update2 = await service.SetJobStatus(
     new JobStatus
     {
         pk = JOB_ID,
         id = "STATUS",
         Status = JobState.INPROGRESS,
         UpdatedAt = DateTime.Now
-    }, JOB_ID)); ;
+    }, JOB_ID);
+Console.WriteLine(update2.Resource);
 
-Console.WriteLine(service.GetJobStatus(JOB_ID)); ;
+var updatedJobStatus2 = await service.GetJobStatus(JOB_ID);
+Console.WriteLine(updatedJobStatus2.Resource);
